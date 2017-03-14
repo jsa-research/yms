ad85e55874b26098c455b0ede7735204feb0f5f1
Alexander Zinchuk <zinchuk@yandex-team.ru>
2017-03-14 20:08:41 +0300

    Extract `handleErrors` to a separate middleware

--- defaults/server.js
+++ defaults/server.js
@@ -6,31 +6,15 @@ const compose = require('koa-compose');
 const cors = require('kcors');
 const clc = require('cli-color');
 const m = require('yms').middleware;
-const PublicError = require('yms').PublicError;
 
 const args = minimist(process.argv);
 const source = args.port || args.p || args.socket || 8000;
 const app = new Koa();
 
 app
-    .use(handleError)
+    .use(m.handleErrors)
     .use(yms())
     .use(cors());
-
-function handleError(ctx, next) {
-    return next()
-        .catch((err) => {
-            console.error(err);
-            
-            const showMessage = ctx.yms && ctx.yms.development || error instanceof PublicError;
-
-            ctx.status = err.status || 500;
-            ctx.body = {
-                error: true,
-                message: showMessage && err.message || 'Internal Server Error'
-            };
-        });
-}
 
 function yms() {
     const router = new Router();
